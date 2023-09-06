561b146f96add3267479f95199cc8292ef93ed12
misbahabroruddin <misbahabroruddin@gmail.com>
2023-09-06 07:59:57 +0700

    add multer for middleware to handle upload photo

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
 const authMiddleware = require('../middlewares/auth');
 const authRouter = require('./auth');
-const getRouter = require('./get');
+const upload = require('./../middlewares/uploadImage');
 const CommentController = require('../controllers/comment.controller');
 const PostController = require('../controllers/post.controller');
 
 router.use('/auth', authRouter);
 router.post('/comments', CommentController.create);
-router.get('/post', PostController.getAll);
-router.get('/post/:id', PostController.getId);
+router.get('/posts', PostController.getAll);
+router.get('/posts/:id', PostController.getId);
 router.use(authMiddleware);
 router.delete('/comments/:id', CommentController.delete);
-router.post('/post', PostController.create);
-router.put('/post/:id', PostController.update);
-router.put('/post/:id', PostController.delete);
+router.post('/posts', upload.single('thumbnail_url'), PostController.create);
+router.put('/posts/:id', PostController.update);
+router.delete('/post/:id', PostController.delete);
 
 module.exports = router;
