b03661c8eecd72d50025be7f19b55b6f2c8ae13f
misbahabroruddin <misbahabroruddin@gmail.com>
2023-09-16 09:39:29 +0700

    add multer middleware for put method

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/me', UserController.getUserLogin);
 router.patch('/me', upload.single('image_url'), UserController.updateUser);
 router.delete('/comments/:id', CommentController.delete);
 router.post('/posts', upload.single('thumbnail_url'), PostController.create);
-router.put('/posts/:id', PostController.update);
-router.delete('/post/:id', PostController.delete);
+router.put('/posts/:id', upload.single('thumbnail_url'), PostController.update);
+router.delete('/posts/:id', PostController.delete);
 
 module.exports = router;
